fd0a2eaa1dacd57ef6e42688cdff0144da007426
840393572@qq.com <11870210+z-seventeen@user.noreply.gitee.com>
2024-01-11 11:12:31 +0800

    fix：开票模块功能

--- src/views/outputInvoice/notInvoice/Api.js
+++ src/views/outputInvoice/notInvoice/Api.js
@@ -3,25 +3,25 @@ import {
   postJSON, download
   } from '@/utils/request'
 
-
+  import { config } from "@/config"
   // 获取列表
-  export const getList = data => postJSON(`/applyInvoiceUpload/queryNotInvoiced`, data)
+  export const getList = data => postJSON(`${config.host}/applyInvoiceUpload/queryNotInvoiced`, data)
 
   // 开票
-  export const openInvoice = (data) => postJSON(`/applyInvoiceUpload/openInvoice`, data)
+  export const openInvoice = (data) => postJSON(`${config.host}/applyInvoiceUpload/openInvoice`, data)
 
   // 删除
-  export const deleteInvoice = (data) => postJSON(`/applyInvoiceUpload/deleteInvoice`, data)
+  export const deleteInvoice = (data) => postJSON(`${config.host}/applyInvoiceUpload/deleteInvoice`, data)
 
   // 校验是否可以开票
-  export const checkOpenInvoice = (data) => postJSON(`/applyInvoiceUpload/checkOpenInvoices`, data)
+  export const checkOpenInvoice = (data) => postJSON(`${config.host}/applyInvoiceUpload/checkOpenInvoices`, data)
 
   /**
    * @desption 导出
    */
-  export const downLoadNoOpenList = data => download('/applyInvoiceUpload/downLoadNoOpenList', data);
+  export const downLoadNoOpenList = data => download(`${config.host}/applyInvoiceUpload/downLoadNoOpenList`, data);
 
   /**
    * @desption 获取当前用户可以开票的开票点（不分页）
    */
-  export const getOrgList = data => postJSON(`/orgnization/getOrgList`, data);
+  export const getOrgList = data => postJSON(`${config.host}/orgnization/getOrgList`, data);
