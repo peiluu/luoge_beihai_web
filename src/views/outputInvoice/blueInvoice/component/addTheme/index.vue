622cf6570cd3aed6f7f624efd9de73ff0140003c
840393572@qq.com <11870210+z-seventeen@user.noreply.gitee.com>
2024-01-11 14:56:07 +0800

    fix:蓝票模块

---
+++ src/views/outputInvoice/blueInvoice/component/addTheme/index.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="">
+        <el-dialog
+            title="提示"
+            :visible = "visible"
+            width="30%"
+            @update:visible="updateVisible"
+            :before-close="handleClose">
+            <span>这是一段信息</span>
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="updateVisible(false)">取 消</el-button>
+                <el-button type="primary" @click="handeOk">确 定</el-button>
+            </span>
+        </el-dialog>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name:'addThemePage',
+    props:{
+     visible:{
+         type:Boolean,
+         default:false
+     },
+ },
+    components: {},
+    data() {
+        return {
+
+        };
+    },
+    computed: {},
+    watch: {},
+    methods: {
+        /* 更新visible */
+      updateVisible(value) {
+        this.$emit('update:visible', value);
+      },
+      
+      /*确定 */
+      handeOk(){
+        this.updateVisible(false)
+      }
+    },
+    created() {},
+    mounted() {},
+    beforeCreate() {},
+    beforeMount() {},
+    beforeUpdate() {},
+    updated() {},
+    beforeDestroy() {},
+    destroyed() {},
+    activated() {},
+    }
+</script>
+<style scoped>
+/*@import url(); 引入公共css类*/
+
+</style>
