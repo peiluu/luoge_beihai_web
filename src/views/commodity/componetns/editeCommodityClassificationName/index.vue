09f8840184cc31781ca0939be535f3a281048457
840393572@qq.com <11870210+z-seventeen@user.noreply.gitee.com>
2024-01-16 18:57:21 +0800

    fix:红票模块

---
+++ src/views/commodity/componetns/editeCommodityClassificationName/index.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="">
+        <el-dialog
+        title="提示"
+        :visible="visible"
+        :width="width"
+        @update:visible="updateVisible"
+        :before-close="handleClose">
+            <span>这是一段信息</span>
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="updateVisible(false)">取 消</el-button>
+                <el-button type="primary" @click="handleConfirm">确 定</el-button>
+            </span>
+        </el-dialog>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name:'',
+    props:{
+        visible: {
+            type: Boolean,
+            default: false,
+        },
+
+        title: {
+            type: String,
+            default: 'title'
+        },
+
+        width: {
+            type: String,
+            default: '30%'
+        }
+    },
+    components: {},
+    data() {
+        return {
+
+        };
+    },
+    computed: {},
+    watch: {},
+    methods: {
+        /* 关闭 */
+        updateVisible(value) {
+            this.$emit('update:visible', value);
+        },
+
+        /* 确认 */
+        handleConfirm(){
+
+            this.updateVisible(false);
+        },
+
+        /* 关闭前 */
+        handleClose(){
+
+        },
+    },
+    created() {},
+    mounted() {},
+    beforeCreate() {},
+    beforeMount() {},
+    beforeUpdate() {},
+    updated() {},
+    beforeDestroy() {},
+    destroyed() {},
+    activated() {},
+    }
+</script>
+<style scoped>
+/*@import url(); 引入公共css类*/
+
+</style>
