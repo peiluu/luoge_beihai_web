f2092d73b3c9f0f72b490244d1604a183fae897a
ex_huxf6 <9527@qq.com>
2024-01-23 15:53:10 +0800

    feat: 新增发票勾选路由

--- src/router/router.config.js
+++ src/router/router.config.js
@@ -35,6 +35,120 @@ export const mainChildrenRoutes = [
     meta: { title: '红票信息申请列表', tab: true },
     component: () => import('@/views/outputInvoice/redInvoice/infoTable/Index.vue'),
   },
+  {
+    path: '/inputInvoice/enterpriseList/Index',
+    name: 'inputInvoiceEnterpriseList',
+    meta: { name: '选择企业' },
+    component: () => import('@/views/inputInvoice/enterpriseList/Index'),
+  },
+  {
+    path: '/inputInvoice/choseDate',
+    name: 'inputInvoiceChoseDate',
+    meta: { name: '属期选择' },
+    component: () => import('@/views/inputInvoice/choseDate/Index'),
+  },
+  {
+    path: '/inputInvoice/waitConfirm',
+    name: 'inputWaitConfirm',
+    meta: { name: '待确认进项红字发票' },
+    component: () => import('@/views/inputInvoice/waitConfirm/Index'),
+  },
+  {
+    path: '/inputInvoice/invoiceDeductionCheck',
+    name: 'invoiceDeductionCheck',
+    meta: { name: '发票勾选抵扣' },
+    component: () =>
+      import('@/views/inputInvoice/invoiceDeductionCheck/Index'),
+  },
+  {
+    path: '/inputInvoice/invoiceNotDeductionCheck',
+    name: 'invoiceNotDeductionCheck',
+    meta: { name: '发票勾选不抵扣' },
+    component: () =>
+      import('@/views/inputInvoice/invoiceNotDeductionCheck/Index'),
+  },
+
+  // 发票下载 - 列表
+  {
+    path: '/invoiceDownload/list',
+    name: 'downLoadList',
+    meta: { name: '发票下载列表' },
+    component: () =>
+      import('@/views/inputInvoice/invoiceDownload/list/Index'),
+  },
+  // 发票下载 - 下载
+  {
+    path: '/invoiceDownload/download',
+    name: 'invoiceDownloadDownload',
+    meta: { name: '下载发票' },
+    component: () =>
+      import('@/views/inputInvoice/invoiceDownload/download/Index'),
+  },
+  // 发票下载 - 历史明细
+  {
+    path: '/invoiceDownload/historyDetail',
+    name: 'historyDetail',
+    meta: { name: '历史下载明细' },
+    component: () =>
+      import('@/views/inputInvoice/invoiceDownload/list/Index'),
+  },
+
+  //  勾选抵扣统计 - 勾选抵扣统计列表
+  {
+    path: '/deductionStatistics/list',
+    name: 'deductionStatisticsList',
+    meta: { name: '勾选抵扣统计' },
+    component: () =>
+      import('@/views/inputInvoice/deductionStatistics/list/Index'),
+  },
+  // 勾选抵扣统计 - 勾选抵扣统计确认 / 申请统计
+  {
+    path: '/deductionStatistics/applyStatistics',
+    // name: 'applyStatistics',
+    meta: { name: '抵扣统计表' },
+    component: () =>
+      import(
+        '@/views/inputInvoice/deductionStatistics/applyStatistics/Index'
+      ),
+  },
+  //  勾选抵扣统计 - 历史抵扣统计与明细
+  {
+    path: '/deductionStatistics/historyStatistics',
+    name: 'historyStatistics',
+    meta: { name: '历史抵扣统计与明细' },
+    component: () =>
+      import(
+        '@/views/inputInvoice/deductionStatistics/historyStatistics/Index'
+      ),
+  },
+  //  勾选抵扣统计 - 历史抵扣统计表
+  {
+    path: '/deductionStatistics/statisticsTable',
+    name: 'statisticsTable',
+    meta: { name: '抵扣统计表' },
+    component: () =>
+      import(
+        '@/views/inputInvoice/deductionStatistics/applyStatistics/Index'
+      ),
+  },
+  //  勾选抵扣统计 - 历史抵扣统计明细
+  {
+    path: '/deductionStatistics/statisticsDetails',
+    name: 'statisticsDetails',
+    meta: { name: '抵扣统计明细' },
+    component: () =>
+      import(
+        '@/views/inputInvoice/deductionStatistics/statisticsDetails/Index'
+      ),
+  },
+
+  // 进项 - 发票查验
+  {
+    path: '/inputInvoice/approveInvoice',
+    name: 'approveInvoice',
+    meta: { name: '发票查验' },
+    component: () => import('@/views/inputInvoice/approveInvoice/Index'),
+  },
 ];
 
 // 页面路由(独立页面), 无公共左侧菜单导航，无公共头部。如：登录页
