2725863236b057dfd9112417374cd259512bc89b
ex_huxf6 <9527@qq.com>
2024-01-24 09:39:49 +0800

    feat: 进项转出管理

---
+++ src/views/inputInvoice/transfer/Index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>进项转出登记</div>
+</template>
+<script>
+export default {
+  name: 'transferIndex'
+}
+</script>
---
+++ src/views/inputInvoice/transferStatistics/Index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>按比例转出进项统计表</div>
+</template>
+<script>
+export default {
+  name: 'transferStatistics'
+}
+</script>
