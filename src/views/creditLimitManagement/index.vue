f4a93b4d48eac6e162e6fb26cd43eec6d1d304bd
840393572@qq.com <11870210+z-seventeen@user.noreply.gitee.com>
2024-02-28 16:29:08 +0800

    fix:蓝票问题

--- src/views/creditLimitManagement/index.vue
+++ src/views/creditLimitManagement/index.vue
@@ -207,8 +207,9 @@ export default {
       if (code === '0') {
         this.creditInfo = data;
         this.quotaDataList.forEach(item => {
-          const value = data[item?.propKey] || (data.invoiceCreditCodeVo || {})[item?.propKey]
-          item.value = value != undefined ? item.isNum ? value + item.unit : this.formatMoney(value) + item.unit : ''
+          const value = data[item?.propKey] || (data.invoiceCreditCodeVo || {})[item?.propKey];
+          console.log(value,"3456")
+          item.value = value != undefined ? item.isNum || value === 0 ? value + item.unit : this.formatMoney(value) + item.unit : ''
         });
 
       }
