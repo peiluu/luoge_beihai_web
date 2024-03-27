903f9d9ec846e3178f033fe3ada8125e2ca4d855
840393572@qq.com <11870210+z-seventeen@user.noreply.gitee.com>
2024-03-27 13:51:33 +0800

    fix: poolPage

--- src/views/inputInvoice/pool/componetns/collectTicketMage/index.vue
+++ src/views/inputInvoice/pool/componetns/collectTicketMage/index.vue
@@ -12,7 +12,7 @@
                     <el-form-item label="发票号码：">
                         <span>{{ typeStatus.type === 'HGJKS'?pushForm.hgjkshm:typeStatus.type === 'DKDJWSPZ'?pushForm.dkdjwspzh:pushForm.fphm }}</span>
                     </el-form-item>
-                    <el-form-item label="开票日期：">
+                    <el-form-item :label="typeStatus.type==='ZZSFP'?'开票日期：':'填发日期'">
                         <span>{{ pushForm.kprq || pushForm.tfrq }}</span>
                     </el-form-item>
                     <!-- <el-form-item label="发票类型：">
