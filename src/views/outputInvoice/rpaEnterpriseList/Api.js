a860a08bf17e8b6f472e108532cf98a17bd187ee
840393572@qq.com <11870210+z-seventeen@user.noreply.gitee.com>
2024-04-15 17:06:55 +0800

    fix:RPA Loagin

--- src/views/outputInvoice/rpaEnterpriseList/Api.js
+++ src/views/outputInvoice/rpaEnterpriseList/Api.js
@@ -1,22 +1,22 @@
 import { postJSON, getJSON } from '@/utils/request';
-
+import { config } from "@/config"
 /**
  * @desption 查询开通RPA的纳税主体
  */
-export const getList = (data) => postJSON('/rpa/queryBodyList', data);
+export const getList = (data) => postJSON(`${config.host}/rpa/queryBodyList`, data);
 
 /**
  * @desption 登录验证
  */
-export const rpaLogin = (data) => postJSON('/rpa/login', data, null, true);
+export const rpaLogin = (data) => postJSON(`${config.host}/rpa/login`, data, null, true);
 
 /**
  * @desption rpa/获取人脸识别二维码
  */
-export const getFaceImg = (data) => postJSON('/rpa/getFaceImg', data, null, true);
+export const getFaceImg = (data) => postJSON(`${config.host}/rpa/getFaceImg`, data, null, true);
 
 /**
  * @desption rpa/获取人脸识别二维码
  */
-export const refreshLoginState = (data) => postJSON('/rpa/refreshLoginState/' + data.nsrsbh);
+export const refreshLoginState = (data) => postJSON(`${config.host}/rpa/refreshLoginState/` + data.nsrsbh);
 
