0af31194d2ee5e2115501730946d0b41e7eddf31
840393572@qq.com <11870210+z-seventeen@user.noreply.gitee.com>
2024-05-21 16:59:32 +0800

    fix:user Page

--- src/views/sys/user.vue
+++ src/views/sys/user.vue
@@ -1,6 +1,7 @@
 <template>
   <el-card shadow="never" class="aui-card--fill" >
     <div class="mod-sys__user">
+      <article>
       <el-form :inline="true" :model="dataForm" @keyup.enter.native="getDataList()">
         <el-form-item>
           <el-input v-model="dataForm.username" :placeholder="$t('user.username')" clearable></el-input>
@@ -9,12 +10,16 @@
           <ren-select v-model="dataForm.gender" dict-type="gender" :placeholder="$t('user.gender')"></ren-select>
         </el-form-item>
         <el-form-item>
-          <ren-dept-tree v-model="dataForm.deptId" :placeholder="$t('dept.title')" :query="true"></ren-dept-tree>
+          <ren-dept-tree ref="userTableSelect" v-model="dataForm.deptId" :placeholder="$t('dept.title')" :query="true"></ren-dept-tree>
         </el-form-item>
         <el-form-item>
-          <el-button @click="getDataList()">{{ $t('query') }}</el-button>
+          <el-button type="primary" @click="getDataList()">{{ $t('query') }}</el-button>
         </el-form-item>
         <el-form-item>
+          <el-button type="" @click="handleRest">重 置</el-button>
+        </el-form-item>
+        
+        <!-- <el-form-item>
           <el-button v-if="$hasPermission('sys:user:save')" type="primary" @click="addOrUpdateHandle()">{{ $t('add') }}</el-button>
         </el-form-item>
         <el-form-item>
@@ -22,19 +27,26 @@
         </el-form-item>
         <el-form-item>
           <el-button v-if="$hasPermission('sys:user:export')" type="info" @click="exportHandle()">{{ $t('export') }}</el-button>
-        </el-form-item>
+        </el-form-item> -->
 
       </el-form>
+    </article>
       <article style="height: 100%">
+        <article class="table_header">
+          <el-button v-if="$hasPermission('sys:user:save')" type="primary" @click="addOrUpdateHandle()">{{ $t('add') }}</el-button>
+          <el-button v-if="$hasPermission('sys:user:delete')" type="danger" @click="deleteHandle()">{{ $t('deleteBatch') }}</el-button>
+          <el-button v-if="$hasPermission('sys:user:export')" type="" @click="exportHandle()">{{ $t('export') }}</el-button>
+        </article>
       <el-table
         v-loading="dataListLoading"
         :data="dataList"
         border
         @selection-change="dataListSelectionChangeHandle"
         @sort-change="dataListSortChangeHandle"
-        height="calc(100vh - 230px)"
+        height="calc(100vh - 285px)"
         style="width: 100%;">
         <el-table-column type="selection" header-align="center" align="center" width="50"></el-table-column>
+        <el-table-column type="index" label="序号" header-align="center" align="center" width="50"></el-table-column>
         <el-table-column prop="username" :label="$t('user.username')" sortable="custom" header-align="center" align="center"></el-table-column>
         <el-table-column prop="deptName" :label="$t('user.deptName')" header-align="center" align="center"></el-table-column>
         <el-table-column prop="email" :label="$t('user.email')" header-align="center" align="center"></el-table-column>
@@ -101,6 +113,22 @@ export default {
   components: {
     AddOrUpdate,
     SetDataPermission
+  },
+  methods:{
+    handleRest(){
+      this.dataForm.username= '';
+      this.dataForm.gender= '';
+      this.$refs.userTableSelect.clearHandle()
+      this.getDataList();
+    },
   }
 }
 </script>
+<style scoped>
+.table_header{
+  border: 1px solid #EBEEF5;
+  border-bottom: none;
+  background-color: #F5F7FA;
+  padding: 10px;
+}
+</style>
