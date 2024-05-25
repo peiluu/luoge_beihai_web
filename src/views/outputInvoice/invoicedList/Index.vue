17d3b9953b10c1348b4b01383214b1f71ccc7ed4
xbox <xingboxin00@163.com>
2024-05-25 14:06:16 +0800

    fix:已开票列表查询税率（多种税率显示多税率、单种税率显示税率）

--- src/views/outputInvoice/invoicedList/Index.vue
+++ src/views/outputInvoice/invoicedList/Index.vue
@@ -163,6 +163,7 @@ export default {
         { title: "税额", width: 100, dataIndex: "hjse", slot: 'hjse', align: 'right' },
         { title: "备注", width: 100, dataIndex: "bz", showTooltip: true},
         { title: "特定业务", width: 100, dataIndex: "tdys", slot: 'tdys' },
+          { title: "税率", width: 60, dataIndex: "slv"},
         { title: "发送记录", width: 80, dataIndex: "sendRecords", fixed: 'right', slot: 'sendRecords',align: 'center' },
         {
           title: "操作",
