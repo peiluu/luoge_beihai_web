d98431298bf165568d00613334945054d523f648
840393572@qq.com <11870210+z-seventeen@user.noreply.gitee.com>
2024-05-28 17:33:14 +0800

    fix:进行认证管理

--- src/views/inputInvoice/constant.js
+++ src/views/inputInvoice/constant.js
@@ -65,7 +65,13 @@ export const purchaserstatusMap = {
   '39': '已报帐',
   '42': '已记账'
 };
-
+/* 入账状态 */
+export const  enterAccountStatus = [
+  {label:'未入账',value:'01'},
+  {label:'入账（企业所得税税前扣除）',value:'02'},
+  {label:'入账（企业所得税不扣除）',value:'03'},
+  {label:'已入账撤销',value:'06'},
+]
 /**
  * @description 勾选失败原因
  */
