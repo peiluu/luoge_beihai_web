cb5397c04e3d9974feb031075ea31a7d632fb9c3
xbox <xingboxin00@163.com>
2024-06-06 12:11:50 +0800

    fix：校验提示、重置按钮

--- src/views/outputInvoice/outputStatistics/searchForm/index.vue
+++ src/views/outputInvoice/outputStatistics/searchForm/index.vue
@@ -124,7 +124,7 @@ export default {
 
     /* 重置 */
     handleRest() {
-      this.searchForm = { time: this.getDateRangeByCondition(1), fppz: "" };
+      this.searchForm = { time: this.getDateRangeByCondition(1), fppz: "",nsrsbh: "9143120071702048XN", };
       this.handleSearch();
     },
     /* 日期取值  1 2 3 4 5 */
